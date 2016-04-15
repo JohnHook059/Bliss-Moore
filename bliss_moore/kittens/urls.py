fd12c33daa6577a7d611e45187e66328ac44e98a
Ivan Kriuchkov <johnhook059@gmail.com>
2016-04-15 23:36:33 +0500

    #5

--- bliss_moore/kittens/urls.py
+++ bliss_moore/kittens/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^news$', views.news, name='news'),
+    url(r'^cats$', views.cats, name='cats'),
+    url(r'^gallery$', views.gallery, name='gallery'),
+    url(r'^forum$', views.forum, name='forum'),
+    url(r'^contacts$', views.contacts, name='contacts'),
 ]
--- bliss_moore/kittens/views.py
+++ bliss_moore/kittens/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main.html')
+
+def news(request):
+    return render(request, 'news.html')
+
+def cats(request):
+    return render(request, 'cats.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def forum(request):
+    return render(request, 'forum.html')
+
+def contacts(request):
+    return render(request, 'contacts.html')

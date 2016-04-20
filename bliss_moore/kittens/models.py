b4e9eae15ada36e0eea859177932631b29ebb9c0
Vitaly Boitsov <boitcov@mail.ru>
2016-04-20 22:46:45 +0500

    #17

--- bliss_moore/kittens/models.py
+++ bliss_moore/kittens/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
+    is_article = models.BooleanField(default=False)
+    is_new = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
--- bliss_moore/kittens/views.py
+++ bliss_moore/kittens/views.py
@@ -7,26 +7,11 @@ def index(request):
 
 
 def news(request):
-    return render(request, 'news.html')
+    return render(request, 'news.html', {'args': Article.objects.all()})
 
 
 def cats(request):
-    array_cats = Cat.objects.all()
-    list_cats = []
-    for cat in array_cats:
-        cat_dict = {}
-        cat_dict['name'] = cat.name
-        cat_dict['color'] = cat.color
-        cat_dict['male'] = cat.male
-        cat_dict['breed'] = cat.breed
-        cat_dict['birthday'] = cat.birthday
-        cat_dict['father'] = cat.father
-        cat_dict['mother'] = cat.mother
-        cat_dict['kitten'] = cat.kitten
-        cat_dict['litter'] = cat.litter
-        cat_dict['status'] = cat.status
-        list_cats.append(cat_dict)
-    return render(request, 'cats.html', {'args': list_cats})
+    return render(request, 'cats.html', {'args': Cat.objects.all()})
 
 
 def gallery(request):
@@ -34,11 +19,6 @@ def gallery(request):
 
 
 def articles(request):
-#    array_articles = Article.object.all()
-#    list_articles = []
-#    for article in array_articles:
-#        list_articles.append([article.title, article.text])
-#    return render(request, 'articles.html', {'args': list_articles})
     return render(request, 'articles.html', {'args': Article.objects.all()})
 
 
